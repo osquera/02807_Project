f0d54ae88c5a2f9d22297d0e968309845a866098
Anton M. Storgaard <antonmosquerastorgaard@gmail.com>
2025-11-11 22:37:57 +0100

    KAN-16 [bugfix]: Fixed download-data task

--- src/02807_project/data/loader.py
+++ src/02807_project/data/loader.py
@@ -1,29 +1,79 @@
-import shutil
+import zipfile
 from pathlib import Path
 
 import kagglehub
+import polars as pl
 
 RAW_LOCATION = Path(__file__).parent / "raw"
 RAW_LOCATION.mkdir(parents=True, exist_ok=True)
 
 # Load the latest version
-large_movie_path = kagglehub.dataset_download(
-    handle="chaitanyahivlekar/large-movie-dataset",
-    path="movies_dataset.csv",
+large_movie_path = Path(
+    kagglehub.dataset_download(
+        handle="chaitanyahivlekar/large-movie-dataset",
+        path="movies_dataset.csv",
+    )
 )
 
-shutil.copy(large_movie_path, RAW_LOCATION / "large_movie_dataset.csv")
+with zipfile.ZipFile(large_movie_path, "r") as zip_ref:
+    zip_ref.extract("movies_dataset.csv", RAW_LOCATION)
 
-rotten_tomatoes_path = kagglehub.dataset_download(
-    handle="stefanoleone992/rotten-tomatoes-movies-and-critic-reviews-dataset",
-    path="rotten_tomatoes_critic_reviews.csv",
+(RAW_LOCATION / "movies_dataset.csv").rename(RAW_LOCATION / "large_movie_dataset.raw.csv")
+
+large_movie_dataset = pl.scan_csv(
+    RAW_LOCATION / "large_movie_dataset.raw.csv",
+    encoding="utf8-lossy",
+    truncate_ragged_lines=True,
+    ignore_errors=True,
+    infer_schema_length=100000,
+    quote_char=None,
+)
+large_movie_dataset.sink_csv(RAW_LOCATION / "large_movie_dataset.csv")
+(RAW_LOCATION / "large_movie_dataset.raw.csv").unlink()
+
+rotten_tomatoes_path = Path(
+    kagglehub.dataset_download(
+        handle="stefanoleone992/rotten-tomatoes-movies-and-critic-reviews-dataset",
+        path="rotten_tomatoes_critic_reviews.csv",
+    )
 )
 
-shutil.copy(rotten_tomatoes_path, RAW_LOCATION / "rotten_tomatoes_critic_reviews.csv")
 
-actors_path = kagglehub.dataset_download(
-    handle="darinhawley/imdb-films-by-actor-for-10k-actors",
-    path="actorfilms.csv",
+with zipfile.ZipFile(rotten_tomatoes_path, "r") as zip_ref:
+    zip_ref.extract("rotten_tomatoes_critic_reviews.csv", RAW_LOCATION)
+
+(RAW_LOCATION / "rotten_tomatoes_critic_reviews.csv").rename(RAW_LOCATION / "rotten_tomatoes_critic_reviews.raw.csv")
+
+rotten_tomatoes_dataset = pl.scan_csv(
+    RAW_LOCATION / "rotten_tomatoes_critic_reviews.raw.csv",
+    encoding="utf8-lossy",
+    truncate_ragged_lines=True,
+    ignore_errors=True,
+    infer_schema_length=100000,
+    quote_char=None,
+)
+rotten_tomatoes_dataset.sink_csv(RAW_LOCATION / "rotten_tomatoes_critic_reviews.csv")
+(RAW_LOCATION / "rotten_tomatoes_critic_reviews.raw.csv").unlink()
+
+actors_path = Path(
+    kagglehub.dataset_download(
+        handle="darinhawley/imdb-films-by-actor-for-10k-actors",
+        path="actorfilms.csv",
+    )
 )
 
-shutil.copy(actors_path, RAW_LOCATION / "actorfilms.csv")
+with zipfile.ZipFile(actors_path, "r") as zip_ref:
+    zip_ref.extract("actorfilms.csv", RAW_LOCATION)
+
+(RAW_LOCATION / "actorfilms.csv").rename(RAW_LOCATION / "actorfilms.raw.csv")
+
+actors_dataset = pl.scan_csv(
+    RAW_LOCATION / "actorfilms.raw.csv",
+    encoding="utf8-lossy",
+    truncate_ragged_lines=True,
+    ignore_errors=True,
+    infer_schema_length=100000,
+    quote_char=None,
+)
+actors_dataset.sink_csv(RAW_LOCATION / "actorfilms.csv")
+(RAW_LOCATION / "actorfilms.raw.csv").unlink()
